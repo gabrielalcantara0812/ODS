4017b35818a5d11c7bb2fa1e3315885f332a5404
Gabriel <gabrielalcantara1823@gmail.com>
2022-10-18 15:55:58 -0300

    objectives responsive, adding modal

--- src/Components/ODS/ODS.js
+++ src/Components/ODS/ODS.js
@@ -5,9 +5,33 @@ import One from '../../assets/1.png'
 import Two from '../../assets/2.png'
 import Seven from '../../assets/7.png'
 import Nine from '../../assets/9.png'
+import Eleven from '../../assets/11.png'
 import Fifteen from '../../assets/15.png'
+import BigOne from '../../assets/big1.png'
+import BigTwo from '../../assets/big2.png'
+import BigSeven from '../../assets/big7.png'
+import BigNine from '../../assets/big9.png'
+import BigEleven from '../../assets/big11.png'
+import BigFifteen from '../../assets/big15.png'
 
 const ODS = () => {
+
+    const [one, setOne] = useState(false)
+
+    const OneText = () => {
+        return(
+            <S.ContainerOne>
+                <S.HeadContainer>
+                    <S.TitleContainer>
+
+                    </S.TitleContainer>
+                    <S.ImageContainer>
+                        <img src={BigOne} alt='1'/>
+                    </S.ImageContainer>
+                </S.HeadContainer>
+            </S.ContainerOne>
+        )
+    }
     return(
         <S.MomContainer>
             <S.Header>
@@ -20,8 +44,26 @@ const ODS = () => {
                 <S.Paragraph>Os Objetivos de Desenvolvimento Sustentável são um apelo global à ação para acabar com a pobreza, proteger o meio ambiente e o clima e garantir que as pessoas, em todos os lugares, possam desfrutar de paz e de prosperidade. Estes são os objetivos para os quais as Nações Unidas estão contribuindo a fim de que possamos atingir a Agenda 2030 no Brasil.</S.Paragraph>
             </S.Text>
             <S.ObjectivesContainer>
-
+                <S.ObjectivesOrange>
+                    <S.Objectives src={One} alt='Um'/>
+                </S.ObjectivesOrange>
+                <S.ObjectivesOrange>
+                    <S.Objectives src={Two} alt='Dois'/>
+                </S.ObjectivesOrange>
+                <S.ObjectivesGreen>
+                    <S.Objectives src={Seven} alt='Sete'/>
+                </S.ObjectivesGreen>
+                <S.ObjectivesGreen>
+                    <S.Objectives src={Nine} alt='Nove'/>
+                </S.ObjectivesGreen>
+                <S.ObjectivesRed>
+                    <S.Objectives src={Eleven} alt='Onze'/>
+                </S.ObjectivesRed>
+                <S.ObjectivesOrange>
+                    <S.Objectives src={Fifteen} alt='Quinze'/>
+                </S.ObjectivesOrange>
             </S.ObjectivesContainer>
+            {OneText()}
         </S.MomContainer>
     )
 }
--- src/Components/ODS/Style.js
+++ src/Components/ODS/Style.js
@@ -81,5 +81,110 @@ export const Paragraph = styled.p`
 `
 
 export const ObjectivesContainer = styled.div`
+    display: flex;
+    padding-top: 2.6vw;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    width: 80%;
+    @media screen and (max-width: 1024px){
+        width: 90%;
+    }
+    @media screen and (max-width: 768px){
+        width: 50%;
+    }
+    @media screen and (max-width: 425px){
+        padding-top: 4vw;
+        width: 65%;
+    }
+    @media screen and (max-width: 320px){
+        width: 75%;
+    }
+`
+
+export const ObjectivesOrange = styled.figure`
+    background: #F57C00;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: 5px 5px 10px;
+    &:hover{
+        cursor: pointer;
+        transform: scale(1.2);
+        transition: 2s;
+    }
+    @media screen and (max-width: 768px){
+        margin-bottom: 5vw;
+    }
+`
+
+export const ObjectivesGreen = styled.figure`
+    background: #43A047;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: 5px 5px 10px;
+    &:hover{
+        cursor: pointer;
+        transform: scale(1.2);
+        transition: 2s;
+    }
+    @media screen and (max-width: 768px){
+        margin-bottom: 5vw;
+    }
+`
+
+export const ObjectivesRed = styled.figure`
+    background: #D32F2F;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: 5px 5px 10px;
+    &:hover{
+        cursor: pointer;
+        transform: scale(1.2);
+        transition: 2s;
+    }
+    @media screen and (max-width: 768px){
+        margin-bottom: 5vw;
+    }
+`
+
+export const Objectives = styled.img`
+    width: 10vw;
+    @media screen and (max-width: 1024px){
+        width: 12vw;
+    }
+    @media screen and (max-width: 768px){
+        width: 20vw;
+    }
+    @media screen and (max-width: 425px){
+        width: 25vw;
+    }
+    @media screen and (max-width: 320px){
+        width: 30vw;
+    }
+`
+
+export const ContainerOne = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const HeadContainer = styled.div`
+    display:flex;
+`
+
+export const TitleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const ImageContainer = styled.figure`
     
 `
